a0707337070385a7c92f34d259ea46641daf1220
seoyeon <wjddowkd456@naver.com>
2023-04-06 23:35:19 +0900

    design: 글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyled = createGlobalStyle`
+${reset} // 초기화css
+
+*{
+  box-sizing: border-box;
+}
+button {
+  margin:0;
+  padding:0;
+}
+a{
+  color:inherit;
+  text-decoration: none;
+}
+a:visited{
+  color:inherit;
+}
+
+.ir-hidden {
+   position: absolute;
+   clip: rect(0 0 0 0);
+   width: 1px;
+   height: 1px;
+   margin: -1px;
+   overflow: hidden;
+}
+
+html{
+  font-size: 62.5%;
+}
+`;
+
 function App() {
-  return <div>초기화면</div>;
+  return (
+    <>
+      <GlobalStyled />
+    </>
+  );
 }
 
 export default App;
