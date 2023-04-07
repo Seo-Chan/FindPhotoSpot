8dbdf5f8e54a9ad1563960c78a40043e34bdd11a
seoyeon <wjddowkd456@naver.com>
2023-04-07 23:49:11 +0900

    feat: Join 마크업

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
 export default function Join() {
-  return <div>Join</div>;
+  return (
+    <main>
+      <h1>회원가입</h1>
+      <form>
+        <label htmlFor='ID'>
+          아이디: <input type='text' id='ID' />
+        </label>
+        <label htmlFor='password'>
+          비밀번호: <input type='password' id='password' />
+        </label>
+      </form>
+      <button type='submit'>다음</button>
+    </main>
+  );
 }
