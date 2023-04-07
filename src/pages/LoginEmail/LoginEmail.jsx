a60c2ff9986c65e7f92c8bb544208ead66e1803d
seoyeon <wjddowkd456@naver.com>
2023-04-07 23:48:57 +0900

    feat: LoginEmail 마크업

--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function LoginEmail() {
-  return <div>LoginEmail</div>;
+  return (
+    <main>
+      <h1>로그인</h1>
+      <form>
+        <label htmlFor='ID'>
+          아이디: <input type='text' id='ID' />
+        </label>
+        <label htmlFor='password'>
+          비밀번호: <input type='password' id='password' />
+        </label>
+      </form>
+      <button type='submit'>확인</button>
+      <Link to='/join'>이메일로 회원가입</Link>
+    </main>
+  );
 }
