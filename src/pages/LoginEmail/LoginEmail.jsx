e60456871ded6bb7ef8fdfa9ce17c537d243253d
seoyeon <wjddowkd456@naver.com>
2023-04-10 22:35:58 +0900

    design: LoginEmail 페이지 스타일 추가
    - Input, Button 컴포넌트 사용
    - 스타일 추가

--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Input from '../../components/common/Input/Input';
+import Button from '../../components/common/Button/Button';
 
-export default function LoginEmail() {
+const Container = styled.main`
+  background-color: var(--ivory);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 34px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 40px;
+  text-align: center;
+  font-size: 2.8rem;
+  font-weight: 500;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 322px;
+  margin: 50px 0 20px;
+`;
+
+const InputFieldset = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 16px;
+  margin-bottom: 50px;
+`;
+
+function LoginEmail() {
   return (
-    <main>
-      <h1>로그인</h1>
-      <form>
-        <label htmlFor='ID'>
-          아이디: <input type='text' id='ID' />
-        </label>
-        <label htmlFor='password'>
-          비밀번호: <input type='password' id='password' />
-        </label>
-      </form>
-      <button type='submit'>확인</button>
+    <Container>
+      <Title>로그인</Title>
+      <LoginForm>
+        <InputFieldset>
+          <Input id='email' labelText='이메일' />
+          <Input type='password' id='password' labelText='비밀번호' />
+        </InputFieldset>
+        <Button size='lg' fontColor='black' txt='확인' />
+      </LoginForm>
       <Link to='/join'>이메일로 회원가입</Link>
-    </main>
+    </Container>
   );
 }
+
+export default LoginEmail;
