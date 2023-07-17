accc32bc3f1257f5e7901956431de566aa94cd42
seoyeon <wjddowkd456@naver.com>
2023-07-17 22:36:55 +0900

    feat: 회원가입 리듀서 추가

--- src/redux/User.js
+++ src/redux/User.js
@@ -9,6 +9,12 @@ export const userSlice = createSlice({
     image: 'https://mandarin.api.weniv.co.kr/Ellipse.png',
   },
   reducers: {
+    SET_USER: (state, action) => {
+      state.email = action.payload.email;
+      state.password = action.payload.password;
+      state.nickname = action.payload.nickname;
+      state.intro = action.payload.intro;
+    },
     loginUser: (state, action) => {
       state.email = action.payload.email;
       state.nickname = action.payload.nickname;
@@ -23,5 +29,5 @@ export const userSlice = createSlice({
   },
 });
 
-export const { loginUser, logoutUser } = userSlice.actions;
+export const { SET_USER, loginUser, logoutUser } = userSlice.actions;
 export default userSlice.reducer;
