8c7a1bbc5253c6ac7baf23058e5d183ea17a8809
seoyeon <wjddowkd456@naver.com>
2023-07-31 22:24:23 +0900

    feat: api 인스턴스생성

---
+++ src/api/api.js
@@ -0,0 +1,107 @@
+import axios from 'axios';
+
+const baseURL = 'http://49.50.172.178:8080/findPhotoSpot-0.0.1-SNAPSHOT';
+
+const authInstance = axios.create({
+  baseURL,
+  headers: { 'Content-type': 'application/json' },
+});
+
+const imgInstance = axios.create({
+  baseURL,
+});
+
+// const formInstance = axios.create({
+//   BASE_URL,
+//   headers: { 'Content-type': 'multipart/form-data' },
+// });
+
+export const emailValid = async (formData) => {
+  try {
+    const response = await authInstance.post('/user/emailvalid', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const nicknameValid = async (formData) => {
+  try {
+    const response = await authInstance.post('/user/nicknamevalid', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const joinSubmit = async (formData) => {
+  try {
+    const response = await authInstance.post('/user', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getMarker = async (formData) => {
+  try {
+    const response = await authInstance.post('/spot/searchAll', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const spotUpload = async (formData) => {
+  try {
+    const response = await authInstance.post('/spot/insertSpot', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const uploadImgs = async (formData) => {
+  try {
+    const response = await imgInstance.post('/image/uploadfiles', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getSpotData = async (formData) => {
+  try {
+    const response = await authInstance.post('/spot/findSpotById', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getSpotReviewData = async (formData) => {
+  try {
+    const response = await authInstance.post('/user/findRecommender', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const heartData = async (formData) => {
+  try {
+    const response = await authInstance.post('/spotLike/like', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const unheartData = async (formData) => {
+  try {
+    const response = await authInstance.post('/spotLike/unLike', formData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
